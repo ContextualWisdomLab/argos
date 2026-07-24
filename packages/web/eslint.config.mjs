064d11f80502bfeb972a54d23b1792731b5927aa
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-07-24 22:53:15 +0000

    ⚡ Bolt: O(N*M) 중첩 반복문을 O(N+M) 투포인터로 최적화 및 CI 오류 수정

--- packages/web/eslint.config.mjs
+++ packages/web/eslint.config.mjs
@@ -1,5 +1,5 @@
-import coreWebVitals from 'eslint-config-next/core-web-vitals'
-import nextTypescript from 'eslint-config-next/typescript'
+import coreWebVitals from 'eslint-config-next/core-web-vitals.js'
+import nextTypescript from 'eslint-config-next/typescript.js'
 
 const eslintConfig = [
   ...coreWebVitals,
