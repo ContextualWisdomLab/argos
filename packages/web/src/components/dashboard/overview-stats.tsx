ff3ccb12e4d5f0a016d93ebce3832e4ca3e4bc30
Seongho Bae <me@seonghobae.me>
2026-08-17 14:03:25 +0900

    fix(test): provide React runtime for overview accessibility test

--- packages/web/src/components/dashboard/overview-stats.tsx
+++ packages/web/src/components/dashboard/overview-stats.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { cn } from '@/lib/utils'
 import { formatTokens, formatCost } from '@/lib/format'
 
